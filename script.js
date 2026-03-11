3c527987ac628f1f40da01ea5e7628b66f1d3d77
CRyan20 <chongryan225@gmail.com>
2026-03-11 23:00:03 +0800

    fin index page wip info pages

---
+++ script.js
@@ -0,0 +1,33 @@
+new Swiper('.swiper', {
+
+  loop: true,
+
+  // Pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicbullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: 
+  {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 15,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+  }
+});
